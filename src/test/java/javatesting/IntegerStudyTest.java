a1aba01971eddf2e547d8fd61c590c1e00c78325
juanjojg06 <juanjobini@gmail.com>
2026-04-27 21:50:36 +0200

    Video 3  probamos el Integer  con el numero 13 y con el valor que espera el 13

--- src/test/java/javatesting/IntegerStudyTest.java
+++ src/test/java/javatesting/IntegerStudyTest.java
@@ -2,12 +2,17 @@ package javatesting;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class IntegerStudyTest {
 
     @Test
     public void integerStudy(){
 
-
+        Integer vuit = new Integer(8);
+        Integer tretze = 13;
+        int esperat = 13;
+        assertEquals(" El nombre tretze val realment 13" , esperat, tretze.intValue() );
     }
 
 }
