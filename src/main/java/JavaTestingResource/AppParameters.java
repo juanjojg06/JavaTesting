f1ecdf5c3787e6661da464555cd64dc2a1731e61
juanjojg06 <juanjobini@gmail.com>
2026-04-28 14:39:29 +0200

    Video 4 Creacion del metodo deployedURL con las Costantes DOMAIN y PORT

--- src/main/java/JavaTestingResource/AppParameters.java
+++ src/main/java/JavaTestingResource/AppParameters.java
@@ -1,4 +1,15 @@
 package JavaTestingResource;
 
 public class AppParameters {
+
+    public  static  final  String DOMAIN = "127.0.0.1";
+    public  static  final  String PORT = "80";
+
+    public  static  String deployedURL(){
+
+        return  "https://"+ DOMAIN + ":" + PORT;
+    }
+
+
+
 }
