058e1332a112cabb3d864238c8e5bafc29494859
juanjojg06 <juanjobini@gmail.com>
2026-05-03 21:59:11 +0200

    Video 4 test del correctPORT y correctUrl con datos correctos

--- src/main/java/JavaTestingResource/AppParametersTest.java
+++ src/main/java/JavaTestingResource/AppParametersTest.java
@@ -5,16 +5,35 @@ import org.junit.jupiter.api.Test;
 
 public class AppParametersTest {
 
-    public  static  final  String DOMAIN_EXPECTED = "10.0.0.43";
+    public  static  final  String DOMAIN_EXPECTED = "127.0.0.1";
 
     public  static  final  String PORT_EXPECTED = "80";
-
+    public  static  final  String URL_EXPECTED =  "https://"+ DOMAIN_EXPECTED + ":" + PORT_EXPECTED;
 @Test
 
 
     public  void domaindURLCorrect(){
 
     Assert.assertEquals( "Retorna la part FQDN " , DOMAIN_EXPECTED , AppParameters.DOMAIN);
+
+    }
+
+
+    @Test
+
+public void correctPORT(){
+
+        Assert.assertEquals( "Retorna el port correcto " , PORT_EXPECTED , AppParameters.PORT);
+
+    }
+
+
+
+    @Test
+
+    public void correctUrl(){
+
+        Assert.assertEquals( "Retorna el port correcto " , URL_EXPECTED , AppParameters.domaindURL() ) ;
 
     }
 
