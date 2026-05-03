ea39ba3c8c83811b70281bce5e8b3b87541a85e6
juanjojg06 <juanjobini@gmail.com>
2026-05-03 22:09:20 +0200

    Video 4 Implementacion SINGLETON PATTERN

--- src/main/java/JavaTestingResource/AppParameters.java
+++ src/main/java/JavaTestingResource/AppParameters.java
@@ -5,6 +5,26 @@ public class AppParameters {
     public  static  final  String DOMAIN = "127.0.0.1";
     public  static  final  String PORT = "80";
 
+    //Implementacion SINGLETON PATTERN
+
+    private static  AppParameters instance ;
+
+    private  AppParameters(){
+
+
+    }
+
+    public AppParameters getInstance(){
+
+        if ( instance == null )
+
+            instance = new AppParameters();
+
+        return  instance;
+    }
+
+
+
     public  static  String domaindURL(){
 
         return  "https://"+ DOMAIN + ":" + PORT;
