2ec2b89a076b10fba24169bab414c2c95f47fd20
juanjojg06 <juanjobini@gmail.com>
2026-05-14 11:08:45 +0200

    Video 6 Creamos un package javatestingArray  con una clase ArrayTest dentro de ella creamos una array y comprovamos si en el index 0 pone 0 y da positivo el test

---
+++ src/test/java/javatesting/arrayTesting/ArrayTest.java
@@ -0,0 +1,14 @@
+package javatesting.arrayTesting;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class ArrayTest {
+
+    @Test
+
+    public void  exempleArraySenzill(){
+    String [] numeros = {"zero" , "un", "dos", "tres" };
+
+    assertEquals("zero" ,numeros[0] );
+    }
+
+}
