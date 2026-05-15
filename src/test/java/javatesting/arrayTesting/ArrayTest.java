0a1e0564b8afe240487e823c0a57b6ff43820989
juanjojg06 <juanjobini@gmail.com>
2026-05-15 16:13:20 +0200

    Video 6 ahora creamos un test que ordena el array pero como es es de string lo hace por caracterisacion

--- src/test/java/javatesting/arrayTesting/ArrayTest.java
+++ src/test/java/javatesting/arrayTesting/ArrayTest.java
@@ -1,14 +1,35 @@
 package javatesting.arrayTesting;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-public class ArrayTest {
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ArrayTest {
+//6:19
     @Test
 
     public void  exempleArraySenzill(){
-    String [] numeros = {"zero" , "un", "dos", "tres" };
+    String [] numeros = { "zero" , "un" , "dos" , "tres"};
 
     assertEquals("zero" ,numeros[0] );
+
+    String nombres = "";
+
+    for (String n : numeros){
+        nombres += n + '|';
+    }
+    assertEquals(nombres,"zero|un|dos|tres|" );
+    }
+
+    @Test
+    public void provesOrdenarArray(){
+        String [] numeros = { "zero" , "un" , "dos" , "tres"};
+        Arrays.sort(numeros);
+        for ( int i = 0; i < numeros.length-1 ; i++){
+            assertTrue(numeros[i].compareTo((numeros[i+1])) < 0 );
+        }
     }
 
 }
