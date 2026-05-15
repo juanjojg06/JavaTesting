54073a5be8a0bf588ad689ebabcd5b3066ee50d2
juanjojg06 <juanjobini@gmail.com>
2026-05-16 01:14:03 +0200

    Video 8 Creamos Un Package Llamado ExceptionTestin con una clase ExeptionsTets y dentro ella creamos un metodo donde creamos un integer inicializado en null

---
+++ src/test/java/javatesting/ExceptionsTesting/ExeptionsTets.java
@@ -0,0 +1,15 @@
+package javatesting.ExceptionsTesting;
+
+import org.junit.Test;
+
+public class ExeptionsTets {
+
+    @Test
+
+    public  void NullPointerTest(){
+
+       Integer edat = null;
+       String edatEnlletres = edat.toString();
+    }
+
+}
