fbf979dbc1ef18375faa8eaee76d090a441000ce
juanjojg06 <juanjobini@gmail.com>
2026-05-15 16:22:07 +0200

    Video 6 ahora creamos otro test  en donde  creamos un array de numero que desde el index 5 hasta el ultimo rellene  y que compruebe si se cumpe

--- src/test/java/javatesting/arrayTesting/ArrayTest.java
+++ src/test/java/javatesting/arrayTesting/ArrayTest.java
@@ -24,12 +24,35 @@ public class ArrayTest {
     }
 
     @Test
-    public void provesOrdenarArray(){
-        String [] numeros = { "zero" , "un" , "dos" , "tres"};
+    public void provesOrdenarArray() {
+        String[] numeros = {"zero", "un", "dos", "tres"};
         Arrays.sort(numeros);
-        for ( int i = 0; i < numeros.length-1 ; i++){
-            assertTrue(numeros[i].compareTo((numeros[i+1])) < 0 );
+        for (int i = 0; i < numeros.length - 1; i++) {
+            assertTrue(numeros[i].compareTo((numeros[i + 1])) < 0);
         }
     }
 
-}
+        @Test
+
+        public void facriArayParcialment() {
+
+        int[] nums = { 0,1,2,3,4,5,6,7,8,9};
+        Arrays.fill(nums,5,10,-1);
+
+
+            for (int i = 5; i < nums.length ; i++) {
+                assertTrue(nums[i] == -1);
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
