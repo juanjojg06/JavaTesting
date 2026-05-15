bdf8ac814bbbe46659d82ec9cdffd2430d4884c4
juanjojg06 <juanjobini@gmail.com>
2026-05-16 01:24:22 +0200

    Video 8 Creamos otro test que añadimos un metodo que recoge las excepciones con un try y un catch

--- src/test/java/javatesting/ExceptionsTesting/ExeptionsTets.java
+++ src/test/java/javatesting/ExceptionsTesting/ExeptionsTets.java
@@ -1,7 +1,7 @@
 package javatesting.ExceptionsTesting;
 
 import org.junit.Test;
-
+import static org. junit. Assert.*;
 public class ExeptionsTets {
 
     @Test (expected = NullPointerException.class)
@@ -10,6 +10,31 @@ public class ExeptionsTets {
 
        Integer edat = null;
        String edatEnlletres = edat.toString();
+
+       assertEquals(18 , edatEnlletres);
+    }
+
+
+
+    @Test
+
+    public  void catchNullPointerTest(){
+
+        Integer edat = null;
+        String edatEnlletres ;
+
+            try {
+     edatEnlletres = edat.toString();
+            }catch (NullPointerException npe){
+
+                edat = 18;
+            }
+
+
+
+
+
+        assertEquals(18 , edatEnlletres);
     }
 
 }
