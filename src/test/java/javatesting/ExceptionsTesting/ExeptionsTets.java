70a0a8945f4d1d606449dbc8adc5959b6dd02b76
juanjojg06 <juanjobini@gmail.com>
2026-05-16 09:25:43 +0200

    Video 8 Creamos 1 nuevos metodos throwingIllegalArgumentsExcepciontryNullPointerTest

--- src/test/java/javatesting/ExceptionsTesting/ExeptionsTets.java
+++ src/test/java/javatesting/ExceptionsTesting/ExeptionsTets.java
@@ -57,13 +57,30 @@ public class ExeptionsTets {
         } finally {
             text = "Edat == " + edatEnlletres;
         }
-
-
-
-
-
         assertEquals("Edat == 18" , text);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public  void throwingIllegalArgumentsExcepciontryNullPointerTest(){
+
+        Integer edat = null;
+        String edatEnlletres="" ;
+        String text;
+
+        try {
+            System.out.println("Paso 1 Generemos NUllPointException");
+            edatEnlletres = edat.toString();
+        }catch (NullPointerException npe){
+            System.out.println("Paso 2 Capturamos la NullPointException");
+            System.out.println("Paso 3 lanzamos una excepcion");
+            throw new IllegalArgumentException("excepcion crea i lanza por mi ");
+        }finally {
+            System.out.println("Paso final codigo de cerramiento");
+        }
+    }
+
+
+
 
 
 
