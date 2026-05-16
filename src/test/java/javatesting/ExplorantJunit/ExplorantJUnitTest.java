12bfa1d8277ed625ee03422626ecbf8299a33a2b
juanjojg06 <juanjobini@gmail.com>
2026-05-16 15:32:53 +0200

    Video 9 Creamos una @BeforeClass  y un nuevo test

--- src/test/java/javatesting/ExplorantJunit/ExplorantJUnitTest.java
+++ src/test/java/javatesting/ExplorantJunit/ExplorantJUnitTest.java
@@ -1,5 +1,6 @@
 package javatesting.ExplorantJunit;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -24,6 +25,19 @@ public void illegalParameterestest(){
     throw  new IllegalArgumentException();
 }
 
+@Test
+    public void missatgeExcepcioTest(){
+    exprction.expect(IllegalArgumentException.class);
+    exprction.expectMessage("the");
+    throw  new IllegalArgumentException("the exceptions");
 
+}
+
+@BeforeClass
+
+    public  static void executaUnCopPerClasseAbansDeQualsevolTets(){
+
+    System.out.println("Antes de ejecutar el test , una vez por clase");
+}
 
 }
