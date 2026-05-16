a01c03ab6b636f2cbc23fa414157d6962fbae86e
juanjojg06 <juanjobini@gmail.com>
2026-05-16 15:19:55 +0200

    Video 9 Creamos un package y una nueva clase llamada ExeptionsTets

--- src/test/java/javatesting/ExceptionsTesting/ExeptionsTets.java
+++ src/test/java/javatesting/ExceptionsTesting/ExeptionsTets.java
@@ -1,5 +1,5 @@
 package javatesting.ExceptionsTesting;
-//14:44
+
 import org.junit.Test;
 import static org. junit. Assert.*;
 public class ExeptionsTets {
---
+++ src/test/java/javatesting/ExplorantJunit/ExplorantJUnitTest.java
@@ -0,0 +1,16 @@
+package javatesting.ExplorantJunit;
+
+import org.junit.Test;
+
+import java.security.InvalidParameterException;
+
+public class ExplorantJUnitTest {
+
+@Test(expected = InvalidParameterException.class)
+
+    public void testedJantIllegalArgumentException(){
+
+
+}
+
+}
